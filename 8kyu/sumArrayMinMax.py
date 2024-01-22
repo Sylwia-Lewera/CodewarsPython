86819bb72551370dec0a7c636cb093efbe91f0f1
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-01-22 21:19:00 +0100

    22-01-2024

--- 8kyu/sumArrayMinMax.py
+++ 8kyu/sumArrayMinMax.py
@@ -1,10 +1,9 @@
 """
-um all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 
 The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
 Mind the input validation.
-
 Input validation
 If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 """
