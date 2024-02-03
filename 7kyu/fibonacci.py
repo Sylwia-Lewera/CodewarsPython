b7293b2af9933a00c5a7db77fdfa7f3716d7d7f6
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-03 14:27:50 +0100

    03-02-2024

--- 7kyu/fibonacci.py
+++ 7kyu/fibonacci.py
@@ -4,6 +4,18 @@ Create function fib that returns n'th element of Fibonacci sequence (classic pro
 def fibonacci(n: int) -> int:
     """Given a positive argument n, returns the nth term of the Fibonacci Sequence.
     """
+    """ recursive solution
+
     if n in {0, 1}:  # Base case
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
