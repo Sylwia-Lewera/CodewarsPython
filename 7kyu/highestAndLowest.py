dfb4f6cc851d7cd7cea1d605c77b54f89d2815a9
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-12 11:29:49 +0100

    12-02-2024

--- 7kyu/highestAndLowest.py
+++ 7kyu/highestAndLowest.py
@@ -5,6 +5,7 @@ Examples
 high_and_low("1 2 3 4 5")  # return "5 1"
 high_and_low("1 2 -3 4 5") # return "5 -3"
 high_and_low("1 9 3 4 -5") # return "9 -5"
+
 Notes
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
