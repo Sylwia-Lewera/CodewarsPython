b4296556b4641db3d862fe9fd34548de6dac5ae9
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-14 11:44:31 +0100

    14-02-2024

---
+++ 6kyu/multiplesOf3Or5.py
@@ -0,0 +1,17 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+"""
+def solution(number):
+    sum = 0
+    if number < 0:
+        return 0
+    for i in range (number):
+        if not i % 3 or not i % 5:
+            sum +=i
+    return sum
--- checkBehaviour.py
+++ checkBehaviour.py
@@ -1,4 +1,13 @@
 
-num = 9119
-numbers = ''.join( [(str(int(x) ** 2)) for x in str(num)])
-print(numbers)
+# initializing string  
+test_string = "Geeksforgeeks is best Computer Science Portal"
+ 
+# printing original string
+print ("The original string is : " +  test_string)
+ 
+# using split()
+# to extract words from string
+res = test_string.split()
+ 
+# printing result
+print ("The list of words is : ",  res)
