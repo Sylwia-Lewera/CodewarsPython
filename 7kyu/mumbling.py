74fd345d49486b11ffc4d62de65020c410070968
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-21 14:59:19 +0100

    21-02-2024

---
+++ 7kyu/mumbling.py
@@ -0,0 +1,16 @@
+"""
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
+def accum(st):
+    result = ""
+    index = 0
+    for item in st:
+        result += f"{item.upper()}{item.lower() * index}-"
+        index += 1
+    return result [:-1]
