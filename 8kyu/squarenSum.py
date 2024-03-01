ffdb282a27b80bda23e894284b7dd35696cfcf55
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-03-01 15:17:42 +0100

    01-03-2024

---
+++ 8kyu/squarenSum.py
@@ -0,0 +1,5 @@
+"""
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+"""
+def square_sum(numbers):
+    return sum([x * x for x in numbers])
