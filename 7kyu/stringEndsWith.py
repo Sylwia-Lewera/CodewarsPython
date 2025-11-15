d0dcd60cd45aed49a4d4f1800d6736c9679888d2
Sylwia Lewera <lewera.sylwia@gmail.com>
2025-11-15 17:41:30 +0100

    7kyu kata after break 15/11/2025

---
+++ 7kyu/stringEndsWith.py
@@ -0,0 +1,16 @@
+"""
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+Inputs: "abc", "bc"
+Output: true
+
+Inputs: "abc", "d"
+Output: false
+
+"""
+
+def solution(text, ending):
+    # your code here...
+    return text.endswith(ending)
